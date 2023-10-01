ed56538dabd50baeb8a6689b5f59ba39b03d1caa
Thamires Andrade <thamires@upoutsourcing.com>
2023-10-01 17:45:56 -0300

    create config styles to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `1<router-outlet></router-outlet>`,
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
   title = 'pokemon-angular';
