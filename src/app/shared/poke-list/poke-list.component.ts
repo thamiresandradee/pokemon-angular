75fc965416c230f8f2ec323b29089751ad52a8ac
Thamires Andrade <thamires@upoutsourcing.com>
2023-10-01 20:28:24 -0300

    create style to list and types

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
