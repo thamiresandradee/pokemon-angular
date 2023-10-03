27f39a11c3dad8cd1151e54d5b6a5202165443d7
Thamires Andrade <thamires@upoutsourcing.com>
2023-10-03 13:12:34 -0300

    remove poke-list on shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,19 +3,16 @@ import { CommonModule } from '@angular/common';
 
 //Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
-import { PokeListComponent } from './poke-list/poke-list.component';
 import { RouterModule } from '@angular/router';
 import { PokeTypesComponent } from './poke-types/poke-types.component';
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
-    PokeListComponent,
     PokeTypesComponent,
   ],
   exports: [
     PokeHeaderComponent,
-    PokeListComponent,
     PokeTypesComponent
   ],
   imports: [
